60e151579fdfe8f850883b62aed21bbac157bc57
Njau Wordpress <njau.wordpress@gmail.com>
2025-06-23 18:07:21 +0300

    Agent Setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "Njau" }));
 
-  return <div className="flex flex-col p-4 gap-y-4">
-    {data?.greeting}
+  return <div>
+    Home View
   </div>;
 };
